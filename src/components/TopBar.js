13f94d71a49ed52f9d2e00d4a27acadf1992930f
ThibautBerg <etu43163@henallux.be>
2021-11-02 16:42:00 +0100

    TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,33 +2,26 @@ import React from 'react';
 import './../css/topBar.css'
 import logo from '../logo.svg'
 
-class SearchForm extends React.Component{
-
-    constructor(props) {
-        super(props);
-    }
-
+class TopBar extends React.Component{
     logout() {
 
     }
 
     render() {
         return (
-            <div className="container-fluid bg-dark">
-                <div className="row">
-                    <div className="col-4">
-                        <img src={logo} alt="logo" width={100}/>
-                    </div>
-                    <div className="col-4">
-                        <h3 className="text-white">WALLONIA FIXED</h3>
-                    </div>
-                    <div className="col-4">
-                        <a href="" onClick={(event => this.logout(event))}><i className="fal fa-sign-out-alt text-white fa-2x"></i></a>
-                    </div>
+            <div id="top-bar" className="row bg-smartcity">
+                <div className="col-4">
+                    <img src={logo} alt="logo" width={100}/>
+                </div>
+                <div className="col-4 text-center">
+                    <h3 className="text-white">WALLONIA FIXED</h3>
+                </div>
+                <div className="col-4 text-right">
+                    <a href="/" onClick={(event => this.logout(event))}><i className="fal fa-sign-out-alt text-white fa-2x"/></a>
                 </div>
             </div>
         );
     }
 }
 
-export default SearchForm;
+export default TopBar;
