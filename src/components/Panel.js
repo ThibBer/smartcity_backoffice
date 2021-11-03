021d5c4b5e95d670a2b6d5b6ea757f54d472c1b5
ThibautBerg <etu43163@henallux.be>
2021-11-03 21:19:16 +0100

    Add multi tables to panel

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,69 +1,99 @@
 import React from 'react';
 import './../css/panel.css'
 import './../css/verticalActionBar.css'
+import UserModal from "./Modals/UserModal";
+import UserTable from "./Tables/UserTable";
+import ReportTable from "./Tables/ReportTable";
+import EventTable from "./Tables/EventTable";
 
-class Panel extends React.Component{
+class Panel extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
+            data: [],
+            currentTable: props.currentButtonChoice ?? "user"
+        }
+    }
 
+    getCurrentTableComponent(){
+        switch(this.state.currentTable){
+            case "user":
+                return <UserTable/>
+            case "report":
+                return <ReportTable/>
+            case "event":
+                return <EventTable/>
+            default:
+                return "";
+        }
+    }
+
+    getTableLabel(name){
+        let label;
+        switch(name){
+            case "user":
+                label = "Utilisateurs";
+                break;
+            case "report":
+                label = "Signalements";
+                break;
+            case "event":
+                label = "Événements";
+                break;
+            case "reportType":
+                label = "Types de signalement";
+                break;
+            default:
+                label = "";
+        }
+
+        return label;
+    }
+
+    getTableIcon(name){
+        switch(name){
+            case "user":
+                return (<i className="far fa-user"/>);
+            case "report":
+                return (<i className="far fa-file-chart-line"/>);
+            case "event":
+                return (<i className="far fa-calendar-week"/>);
+            case "reportType":
+                return (<i className="far fa-list"/>);
+            default:
+                return "";
         }
     }
 
     render() {
         return (
             <div id="panel" className="p-3">
+                <div className="row text-center">
+                    <div className="col">
+                        <h3 id="table-label">{this.getTableIcon(this.state.currentTable)} {this.getTableLabel(this.state.currentTable)}</h3>
+                    </div>
+                </div>
                 {/*HEADER*/}
                 <div className="row">
                     <div className="col">
-                        <button className="btn btn-smartcity rounded-pill"><i className="far fa-plus-circle"/>&nbsp;&nbsp;Ajouter un élément</button>
+                        <button className="btn btn-smartcity rounded-pill"><i
+                            className="far fa-plus-circle"/>&nbsp;&nbsp;Ajouter un élément
+                        </button>
                     </div>
                 </div>
 
                 {/*TABLE*/}
                 <div className="row">
                     <div className="col">
-                        <table id="panel-table" className="table table-striped table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        {this.getCurrentTableComponent()}
                     </div>
                 </div>
+
+                <UserModal/>
             </div>
+
         );
     }
 }
