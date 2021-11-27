5452d078eae54d8a4c96b60e3d0e213365d7a1bd
ThibautBerg <etu43163@henallux.be>
2021-11-27 11:47:12 +0100

    Add callback for custom error message

--- src/components/ErrorCodeManager.js
+++ src/components/ErrorCodeManager.js
@@ -1,18 +1,25 @@
-module.exports.message = (error) => {
-    const errorData = error?.response.data?.error;
-    let errorMessage = "Une erreur inattendue est survenue ...";
+module.exports.message = (error, customChecksCallback) => {
+    let errorMessage = undefined;
 
     if (error?.message === "Network Error") {
         errorMessage = "Une erreur serveur rend cette action impossible, il est impossible de joindre le serveur";
-    }else if(errorData) {
-        errorMessage = errorData;
-    } else if (error.status >= 200 && error.status < 300) {
-        errorMessage = undefined;
-    } else if (error.status >= 400 && error.status < 500) {
-        errorMessage = "Une erreur est survenue. Utilisation d'une donnée incorrecte ou non disponible " + error.response.data;
-    } else if (error.status >= 500 && error.status < 600) {
-        errorMessage = "Une erreur serveur est survenue. Réessayer dans quelques instants";
+    }else if (customChecksCallback !== undefined){
+        errorMessage = customChecksCallback(error);
     }
 
-    return errorMessage;
+    if(errorMessage === undefined){
+        const errorData = error?.response.data?.error;
+
+        if(errorData) {
+            errorMessage = errorData;
+        } else if (error.status >= 200 && error.status < 300) {
+            errorMessage = undefined;
+        } else if (error.status >= 400 && error.status < 500) {
+            errorMessage = "Une erreur est survenue. Utilisation d'une donnée incorrecte ou non disponible " + error.response.data;
+        } else if (error.status >= 500 && error.status < 600) {
+            errorMessage = "Une erreur serveur est survenue. Réessayer dans quelques instants";
+        }
+    }
+
+    return errorMessage ?? "Une erreur inattendue est survenue ...";
 }
